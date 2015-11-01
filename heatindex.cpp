b108df4dc15b816bdce44361ab7788313ded4d70
billygr <billygr@users.noreply.github.com>
2015-11-01 13:35:45 +0000

    Fixed heatindex based on NOAA calculations

--- heatindex.cpp
+++ heatindex.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 
 /* T is temperature in degrees Fahrenheit,
-*  R is relative humidity in percent
+*  RH is relative humidity in percent
 *  source https://en.wikipedia.org/wiki/Heat_index
 *  http://www.wpc.ncep.noaa.gov/html/heatindex.shtml
+*  http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
 */
-double Heat_Index(double T, double R)
+double Heat_Index(double T, double RH)
 {
 	const double C1 = -42.379;
 	const double C2 = 2.04901523;
@@ -16,8 +21,38 @@ double Heat_Index(double T, double R)
 	const double C7 = 1.22874e-3;
 	const double C8 = 8.5282e-4;
 	const double C9 = -1.99e-6;
-	return C1 + C2 * T + C3 * R + C4 * T * R + C5 * T * T + C6 * R * R +
-	    C7 * T * T * R + C8 * T * R * R + C9 * T * T * R * R;
+	double adjustment = 0;
+	double heatindex = 0;
+
+/* Try the simple formula first */
+	heatindex = 0.5 * (T + 61.0 + ((T - 68.0) * 1.2) + (RH * 0.094));
+
+	if (round(heatindex) > 80) {
+/* If the RH is less than 13% and the temperature 
+   is between 80 and 112 degrees F
+*/
+		if (round(RH) < 13) {
+			if ((round(T) > 80) || round(T) < 112) {
+				adjustment =
+				    ((13 - RH) / 4) * sqrt(17 -
+							   abs(T - 95.)) / 17;
+			}
+		}
+
+/* if the RH is greater than 85% and the temperature
+   is between 80 and 87 degrees F
+*/
+		if (round(RH) > 85) {
+			if ((round(T) > 80) || round(T) < 87) {
+				adjustment = ((RH - 85) / 10) * ((87 - T) / 5);
+			}
+		}
+
+		heatindex = C1 + C2 * T + C3 * RH + C4 * T * RH + C5 * T * T +
+		    C6 * RH * RH + C7 * T * T * RH + C8 * T * RH * RH +
+		    C9 * T * T * RH * RH - adjustment;
+	}
+	return heatindex;
 }
 
 double Temperature_F(double C)
